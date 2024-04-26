ffdcdcf54876fef1af195c751195e365641f609b
Ítalo Gregório <italo-07@hotmail.com>
2024-04-26 20:37:05 -0300

    Created Eureka server for microservices discovery

---
+++ app-feature-server/src/main/java/com/ibgreg/appfeatureserver/AppFeatureServerApplication.java
@@ -0,0 +1,15 @@
+package com.ibgreg.appfeatureserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class AppFeatureServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFeatureServerApplication.class, args);
+	}
+
+}
---
+++ app-feature-server/src/test/java/com/ibgreg/appfeatureserver/AppFeatureServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibgreg.appfeatureserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFeatureServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
